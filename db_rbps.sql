e1f11ef076fe670473ef5767cebd3f2ac4979f9a
lipop <873946453@qq.com>
2017-10-16 23:28:50 +0800

    数据库

---
+++ db_rbps.sql
@@ -0,0 +1,71 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.1.49-community : Database - db_rbps
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_rbps` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `db_rbps`;
+
+/*Table structure for table `t_auth` */
+
+DROP TABLE IF EXISTS `t_auth`;
+
+CREATE TABLE `t_auth` (
+  `authId` int(11) NOT NULL AUTO_INCREMENT,
+  `authName` varchar(20) DEFAULT NULL,
+  `authPath` varchar(100) DEFAULT NULL,
+  `parentId` int(11) DEFAULT NULL,
+  `authDescription` varchar(200) DEFAULT NULL,
+  `state` varchar(20) DEFAULT NULL,
+  `iconCls` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`authId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_auth` */
+
+/*Table structure for table `t_role` */
+
+DROP TABLE IF EXISTS `t_role`;
+
+CREATE TABLE `t_role` (
+  `roleId` int(11) NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(20) DEFAULT NULL,
+  `authIds` varchar(50) DEFAULT NULL,
+  `roleDescription` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`roleId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_role` */
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `userType` tinyint(4) DEFAULT NULL,
+  `roleId` int(11) DEFAULT NULL,
+  `userDescription` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`userId`),
+  KEY `FK_t_user` (`roleId`),
+  CONSTRAINT `FK_t_user` FOREIGN KEY (`roleId`) REFERENCES `t_role` (`roleId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
